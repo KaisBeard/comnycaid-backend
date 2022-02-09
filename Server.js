8878a3700832fcbdece7f12eab9118e175cfc7eb
Kai <frenk.el.kai@gmail.com>
2022-02-09 16:25:43 +0100

    unsafe commit

--- Server.js
+++ Server.js
@@ -27,7 +27,7 @@ const httpServer = createServer(server);
 
 const io = new Server(httpServer, {
 	cors: {
-		origin: ["https://kaisbeard.github.io/comnycaid/", "https://tybe.herokuapp.com/", "https://kaisbeard.github.io/" ],
+		origin: "*" , //["https://kaisbeard.github.io/comnycaid/", "https://tybe.herokuapp.com/", "https://kaisbeard.github.io/" ]
 		methods: ["GET", "Post", "Put", "Delete"],
 	},
 });
