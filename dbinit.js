b182ef7a9d88e7db8b48c19531253c43fe1d7a24
Kai <frenk.el.kai@gmail.com>
2022-05-20 16:47:33 +0200

    comments revised

--- dbinit.js
+++ dbinit.js
@@ -1,10 +1,8 @@
-import mongoose from 'mongoose';
-//const mongoose = require("mongoose");
+//import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
   const conn = await mongoose.connect(process.env.MONGO_URI);
-  
   console.log(`Mongo DB connected ${conn.connection.host}`);
 }
 
